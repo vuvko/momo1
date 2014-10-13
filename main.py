3de46b42bdf2f3cc91df37431b540f88dd2d3316
Andrei Shadrikov <6129086+vuvko@users.noreply.github.com>
2014-10-13 17:59:57 +0400

    Done with algorithm. Now testing ang report.

--- main.py
+++ main.py
@@ -1,27 +1,76 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import check_grad
 
 from minfuncs import min_golden, min_wolfe
 
 
 def f1(x):
-    return 0.2 * x * np.log(x) + (x - 2.3) ** 2
+    b = 2
+    y = - x / (x ** 2.0 + b)
+    dy = (x ** 2 - b) / ((x ** 2 + b) ** 2.0)
+    return y, dy
+
+
+def f2(x):
+    b = 0.01
+    l = 39
+    if x <= 1 - b:
+        f0 = 1 - x
+        df0 = -1
+    elif x >= 1 + b:
+        f0 = x - 1
+        df0 = 1
+    else:
+        f0 = (0.5 / b) * (x - 1) ** 2 + b / 2
+        df0 = (x - 1.0) / b
+    y = f0 + ((2 - 2 * b) / (l * np.pi)) * np.sin(x * l * np.pi / 2)
+    dy = df0 + (1 - b) * np.cos(x * l * np.pi / 2)
+    return y, dy
+
+
+def f3(x):
+    return
+
+
+def f4(x):
+    y = -(x + 1) ** 2 + 1
+    dy = -2 * x
+    return y, dy
+
+
+def f5(x):
+    y = -np.sqrt(x + 1) + 1
+    dy = -1.0 / (2 * np.sqrt(x + 1))
+    return y, dy
+
+
+#def f(x):
+#    return - x / (x ** 2.0 + 2)
+#
+#
+#def df(x):
+#    return (x ** 2 - 2) / ((x ** 2 + 2) ** 2.0)
 
 
 def plot_func(func, a, b):
     x = np.arange(a, b, (b - a) / 100.0)
-    y = func(x)
-    plt.plot(x, y, 'b-')
+    Y = np.array([func(e) for e in x])
+    print(Y.shape)
+    plt.plot(x, Y[:, 0], 'b-')
     plt.draw()
 
 
 def main():
-    l = 0.5
-    u = 2.5
+    func = f5
+    alpha0 = 1
+    alpha_max = 3.5
     plt.figure()
-    plot_func(f1, l, u)
-    x_min, f_min, exitflag, num_oracle = min_golden(f1, (l, u), display=1)
-    print('Final result: x_min =', x_min, 'f_min =', f_min, 'num_oracle =', num_oracle)
+    plot_func(func, 0, alpha_max)
+    x_min, f_min, exitflag, num_oracle = min_wolfe(func, alpha0, c1=0.85, c2=0.91, alpha_max=alpha_max, display=1)
+    print('Final result: x_min =', x_min, 'f_min =', f_min, 'exitflag =', exitflag, 'num_oracle =', num_oracle)
     plt.show()
 
 if __name__ == '__main__':
--- minfuncs.py
+++ minfuncs.py
@@ -1,16 +1,55 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def plot_points(a, b, fa, fb):
     plt.plot(a, fa, 'rx')
-    plt.plot(b, fb, 'go')
+    #plt.plot(b, fb, 'go')
     plt.draw()
     #plt.show(block=0)
 
 
-def min_parabolic_step(func, bounds, display=0):
-    return bounds, num_oracle
+def plot_wolfe(f0, df0, alpha_max, c1):
+    x = np.array([0, alpha_max])
+    y = np.array([f0, f0 + c1 * alpha_max * df0])
+    plt.plot(x, y, 'r-')
+    plt.draw()
+
+
+def min_parabolic_step_3p(func, bounds, display=0):
+    x1, fx1, x2, fx2, x3, fx3 = bounds
+    A = np.zeros((3, 3))
+    A[0, :] = np.array([x1 ** 2, x1, 1])
+    A[1, :] = np.array([x2 ** 2, x2, 1])
+    A[2, :] = np.array([x3 ** 2, x3, 1])
+    b = np.array([fx1, fx2, fx3])
+    try:
+        coeff = np.linalg.solve(A, b)
+    except np.linalg.linalg.LinAlgError:
+        coeff = np.ones(3)
+        coeff[1] = -(x1 + x2)
+    x_min = -coeff[1] / (2.0 * coeff[0])
+    f_min = func(x_min)
+    return x_min, f_min, 1
+
+
+def min_parabolic_step_2p(func, bounds, display=0):
+    x1, fx1, dfx1, x2, fx2, dfx2 = bounds
+    A = np.zeros((3, 3))
+    A[0, :] = np.array([x1 ** 2, x1, 1])
+    A[1, :] = np.array([x2 ** 2, x2, 1])
+    A[2, :] = np.array([2 * x1, 1, 0])
+    b = np.array([fx1, fx2, dfx1])
+    try:
+        coeff = np.linalg.solve(A, b)
+    except np.linalg.linalg.LinAlgError:
+        coeff = np.ones(3)
+        coeff[1] = -(x1 + x2)
+    x_min = -coeff[1] / (2.0 * coeff[0])
+    f_min = func(x_min)
+    return x_min, f_min, 1
 
 
 def min_cubic_step(func, bounds, display=0):
@@ -74,39 +113,168 @@ def min_golden(func, bounds, tol_x=1e-5, max_iter=500, display=0):
     return x_min, f_min, exitflag, num_oracle
 
 
-def wolfe_condition(f0, df0, a, dfa, c1, c2):
+def wolfe_condition(f0, df0, a, fa, dfa, c1, c2):
     flag = 0
     if fa <= f0 + c1 * a * df0:
         flag = flag + 1
-    if np.abs(dfa) >= c2 * df0:
+    if np.abs(dfa) <= c2 * abs(df0):
         flag = flag + 2
     return flag
 
 
 def min_wolfe(func, alpha0, c1=1e-4, c2=0.1, alpha_max=100, 
-  tol_interval=1e-8, max_iter=500, display=0):
+  tol_interval=1e-8, max_iter=10, display=0):
+    eps = tol_interval
     k = (np.sqrt(5) - 1) / 2.0
     a = 0
     f0, df0 = func(0)
     fa, dfa = f0, df0
     b = alpha0
     fb, dfb = func(b)
-    x = w = v = (a + b) / 2.0
-    fx, dfx = func(x)
-    fw = fv = fx
-    dfw = dfv = dfx
+    x = v = w = b / 2.0
+    fw, dfw = func(w)
+    fx = fv = fw
+    dfx = dfv = dfw
     exitflag = 2
     num_oracle = 3
-    wolfe_flag0 = wolfe_condition(f0, df0, b, dfb, c1, c2)
-    wolfe_flag1 = wolfe_condition(f0, df0, x, dfx, c1, c2)
-    if wolfe_flag0 = 1 or wolfe_flag1 = 1:
-        wolfe_flag = 1
-    else:
-        wolfe_flag = 0
+    wolfe_flag = wolfe_condition(f0, df0, b, fb, dfb, c1, c2)
+    if wolfe_flag == 1 and dfb < 0:
+        wolfe_flag = -1
+    alpha_min = b
+    phi_min = fb
+    
+    plot_wolfe(f0, df0, alpha_max, c1)
+    
+    int_len = b - a
     for k in range(max_iter):
         if display:
             print('Iteration #', k)
-        if wolfe_flag: # ищем точку, где выполняется первое условие Вольфа
-            
-        else: # ищем точку, где вдобавок выполняется второе условие Вольфа
+            print('  a =', a, 'b =', b, 'w =', w, 'v =', v)
+            print('  x =', x, 'fx =', fx, 'dfx =', dfx)
+        if wolfe_flag == -1: 
+            print('  Going right')
+            # ищём правый край
+            x = b * 2
+            #b = 2 * b
+            if x > alpha_max:
+                alpha_min = b
+                phi_min = fb
+                exitflag = 3
+                break
+            fx, dfx = func(x)
+            num_oracle = num_oracle + 1
+            wolfe_flag = wolfe_condition(f0, df0, x, fx, dfx, c1, c2)
+            int_len = x - b
+            if wolfe_flag == 1 and dfx < 0 and fx < fb:
+                wolfe_flag = -1
+                b, fb, dfb = x, fx, dfx
+            else:
+                t, ft, dft = b, fb, dfb
+                b, fb, dfb = x, fx, dfx
+                x, fx, dfx = t, ft, dft
+        #elif wolfe_flag == 0 or wolfe_flag == 2: 
+        #if wolfe_flag != 1
+        #    # ищем точку, где выполняется первое условие Вольфа
+        #    print('  Now 1st')
+        #    
+        else:
+            # ищем точку, где вдобавок выполняется второе условие Вольфа
+            u1_flag = 0
+            u2_flag = 0
+            if x != v and dfx != dfv:
+                if display:
+                    print('  u1:')
+                # параболическая аппроксимация
+                u1, fu1, no_plus = min_parabolic_step_2p(func, (x, fx, dfx, v, fv, dfv), display)
+                num_oracle = num_oracle + 1
+                if (u1 > a + eps and u1 < b - eps and
+                  abs(u1 - x) < (int_len / 1.5) and
+                  not (wolf_flag != 1 and u1 > x)):
+                    if display:
+                        print('     yep.')
+                    u1_flag = 1
+            if x != w and dfx != dfw:
+                if display:
+                    print('  u2:')
+                # параболическая аппроксимация
+                u2, fu2, no_plus = min_parabolic_step_2p(func, (x, fx, dfx, w, fw, dfw), display)
+                num_oracle = num_oracle + 1
+                if (u2 > a + eps and u2 < b - eps and
+                  abs(u2 - x) < (int_len / 1.5) and
+                  not (wolfe_flag != 1 and u2 > x)):
+                    if display:
+                        print('     yep.')
+                    u2_flag = 1
+            if u1_flag and u2_flag:
+                if abs(u2 - x) < abs(u1 - x):
+                    if display:
+                        print('  get u2')
+                    u = u2
+                else:
+                    if display:
+                        print('  get u1')
+                    u = u1
+            elif u1_flag:
+                if display:
+                    print('  get u1')
+                u = u1
+            elif u2_flag:
+                if display:
+                    print('  get u2')
+                u = u2
+            else:
+                if display:
+                    print('  No u:')
+                if dfx > 0 or wolfe_flag != 1:
+                    if display:
+                        print('       [a, x]')
+                    u = (a + x) / 2.0
+                else:
+                    if display:
+                        print('       [x, b]')
+                    u = (x + b) / 2.0
+            if abs(u - x) < eps:
+                u = x + np.sign(u - x) * eps
+            int_len = abs(u - x)
+            fu, dfu = func(u)
+            if display:
+                print('  u =', u, 'f_u =', fu)
+            num_oracle = num_oracle + 1
+            wolfe_flag_u = wolfe_condition(f0, df0, u, fu, dfu, c1, c2)
+            if fu < fx:
+                if u < x:
+                    b = x
+                elif wolfe_flag == 1:
+                    a = x
+                x, fx, dfx = u, fu, dfu
+            else:
+                if u < x:
+                    if wolfe_flag_u == 1:
+                        a = u
+                    else:
+                        x, fx, dfx = u, fu, dfu
+                else:
+                    b = u
+            wolfe_flag = wolfe_condition(f0, df0, x, fx, dfx, c1, c2)
+        if display:
+            print('  wolfe:', wolfe_flag)
+        if wolfe_flag == 3:
+            alpha_min = x
+            phi_min = fx
+            exitflag = 0
+            break
+        if int_len < eps:
+            alpha_min = x
+            phi_min = fx
+            exitflag = 1
+            break
+        if v != x:
+            w, fw, dfw = v, fv, dfv
+            v, fv, dfv = x, fx, dfx
+        if display:
+            plot_points(x, 0, fx, 0)
+    if display:
+        print('Done.')
+        plt.plot(alpha_min, phi_min, 'go')
+        plt.draw()
     return alpha_min, phi_min, exitflag, num_oracle
